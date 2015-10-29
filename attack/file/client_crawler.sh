031f48cde2f02fd6c1e258cd7659852d60cc2cad
hxwang <maggiewangsjtu@gmail.com>
2015-10-29 12:44:26 -0700

    revise

--- attack/file/client_crawler.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for i in {1..10000}
-do
-	ts=`date -u`
-	wget -q target-ip -O index.html
-	echo "$ts, $i"	
-	sleep 1
-
-done
--- attack/readme.md
+++
@@ -1,3 +0,0 @@
-
-## Scripts
-- [client crawler](./file/client_crawler.sh)
